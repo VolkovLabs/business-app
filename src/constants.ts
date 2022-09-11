a4799e44bc578b068d622fd2c08b066769e9d9b3
Mikhail <mikhail@volkovlabs.io>
2022-09-11 16:19:24 -0400

    Add YouTube Playlist and Docker information

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@
 export const Application = {
   root: '/a/volkovlabs-app',
   name: 'Volkov Labs',
-  subTitle: 'Custom plugin development for Grafana',
+  subTitle: 'Plugin development for Grafana',
 };
